8cbe1a6a56cd06e2688c5048eb380f2c9e6fa140
xeltyx <83575587+xeltyx@users.noreply.github.com>
2022-01-08 15:08:19 +0700

    Baca deskripsi
    republish ulang + nambahin colum harga barang perbiji di cart + ngerapihin data grid di admin biar widthnya rata semua

--- Proyek/Program/ProyekSDP/cartpage.xaml.cs
+++ Proyek/Program/ProyekSDP/cartpage.xaml.cs
@@ -40,7 +40,7 @@ namespace ProyekSDP
         public void loadCart()
         {
             conn.conn.Open();
-            MySqlCommand cmd = new MySqlCommand($"SELECT BARANG.NAMA_BARANG, CART.JUMLAH, CART.JUMLAH * BARANG.HARGA AS SUBTOTAL FROM BARANG, CART WHERE CART.ID_BARANG = BARANG.ID AND CART.USERNAME = '{user.username}'", conn.conn);
+            MySqlCommand cmd = new MySqlCommand($"SELECT BARANG.NAMA_BARANG,CART.JUMLAH,BARANG.HARGA AS \"Harga per barang\",CART.JUMLAH * BARANG.HARGA AS TOTAL FROM BARANG, CART WHERE CART.ID_BARANG = BARANG.ID AND CART.USERNAME = '{user.username}'", conn.conn);
             MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
             dt = new DataTable();
             sda.Fill(dt);
